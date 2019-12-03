9dd36c5baaa9cfe80a9680357bb55f7bae45431c
Dianna A Toledo <56136290+DiannaToledoSG@users.noreply.github.com>
2019-12-03 12:21:14 -0800

    Add files via upload
    #2

--- SimpleSheets/src/simpleSheetsPackage/Main.java
+++ SimpleSheets/src/simpleSheetsPackage/Main.java
@@ -32,6 +32,40 @@ public class Main {
 			String orderDate = create.nextLine();
 			System.out.println("Order Date: " +  orderDate);
 			
+			//Customer name info
+			System.out.println("Enter Customer Name: ");
+			String customerName = create.nextLine();
+			System.out.println("Customer Name: " +  customerName);
+			
+			//String sales Associate info
+			System.out.println("Enter Sales Associate: ");
+			String salesAssociate = create.nextLine();
+			System.out.println("Sales Associate: " +  salesAssociate);
+			
+			//String Billing Address info
+			System.out.println("Enter Billing Address: ");
+			String billingAddress = create.nextLine();
+			System.out.println("Billing Address: " +  billingAddress);
+			
+			//String Shipping Address info
+			System.out.println("Enter Shipping Address: ");
+			String shippingAddress = create.nextLine();
+			System.out.println("Shipping Address: " +  shippingAddress);
+			
+			//String Product name info
+			System.out.println("Enter Product Name: ");
+			String productName = create.nextLine();
+			System.out.println("Product Name: " +  productName);
+			
+			//Double Amount ordered info
+			System.out.println("Enter Amount Ordered: ");
+			double amountOrdered = create.nextDouble();
+			System.out.println("Amount Ordered: " +  amountOrdered);
+			
+			//Double Price info
+			System.out.println("Enter Total Price: ");
+			double price = create.nextDouble();
+			System.out.println("Total Price: " +  price );
 			
 
 	}
